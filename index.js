7138aa39b70a3ff3fec69b96c05e2ff0523854e7
Jeni Dang <jenipdang@yahoo.com>
2021-09-17 22:42:16 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = ("Jeni is the champion");
+
+document.body.append("h1");
